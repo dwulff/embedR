e76118c7a749214ef81002b0385f2285742e40ce
Dirk Wulff <dwulff@users.noreply.github.com>
2023-11-09 14:09:56 +0100

    cmix & big fix

--- R/plot.R
+++ R/plot.R
@@ -6,13 +6,21 @@
 #' @param dimensions an \code{integer} vector determining the dimensions plotted. Default is \code{c(1, 2)}.
 #' @param color an optional \code{character} string specifying the column used to determine point (and label) color.
 #' @param size an optional \code{character} string specifying the column used to determine point size.
+#' @param point_outline an optional \code{character} specifying the color of an optional point outline.
+#' @param point_padding an optional \code{numeric} specifying the point padding.
+#' @param point_alpha an optional \code{numeric} specifying the transparency of points.
 #' @param label an optional \code{character} string specifying the column used to determine label texts.
 #' @param label_size an optional \code{character} string specifying the column used to determine label size.
+#' @param label_color an optional \code{numeric} altering the label color relative to point color. Values of \code{label_color > 0} renders labels darker and \code{label_color < 0} lighter. Argument \code{label_color} must be between -1 and 1.
+#' @param label_outline an optional \code{numeric} specifying the width of an optional white outline around the text labels. The default is \code{label_outline = NULL} implying no outline.
 #' @param label_filter an optional \code{logical} comparison determining which label texts to show in the plot. Can be based on any columns in \code{data}.
-#' @param pt_padding a \code{numeric} specifying the point padding.
-#' @param box_padding a \code{numeric} specifying the label box padding.
-#' @param viridis_set a \code{character} string determining the viridis color set. See \link[ggplot2]{scale_color_viridis_d}.
-#' @param viridis_limits a \code{numeric} vector of length 2 determining the \code{begin} and \code{end} arguments of \link[ggplot2]{scale_color_viridis_d}.
+#' @param label_padding an optional \code{numeric} specifying the label box padding.
+#' @param label_overlaps an optional \code{integer} specifying when to exclude text labels that overlap too many things. Defaults to 10.
+#' @param segment_size an optional \code{numeric} specifying the width of the segments.
+#' @param segment_type an optional \code{numeric} specifying the type of the segments.
+#' @param viridis_set an optional \code{character} string determining the viridis color set. See \link[ggplot2]{scale_color_viridis_d}.
+#' @param viridis_limits an optional \code{numeric} vector of length 2 determining the \code{begin} and \code{end} arguments of \link[ggplot2]{scale_color_viridis_d}.
+#' @param ... further arguments passed to \link[ggplot2]{geom_point}.
 #'
 #' @return The function returns a \code{ggplot2} object.
 #'
@@ -35,20 +43,28 @@ plot.embedR_tbl <- function(data,
                             dimensions = c(1, 2),
                             size = NULL,
                             color = NULL,
+                            point_outline = NULL,
+                            point_padding = .01,
+                            point_alpha = NULL,
                             label = NULL,
                             label_size = 3,
+                            label_color = NULL,
+                            label_outline = NULL,
                             label_filter = NULL,
-                            pt_padding = .02,
-                            box_padding = .05,
+                            label_padding = .02,
+                            label_overlaps = 10,
+                            segment_size = .5,
+                            segment_type = 1,
                             viridis_set = "E",
-                            viridis_limits = c(0,.9)){
+                            viridis_limits = c(0,.9),
+                            ...){
 
   # run data
   if(!any(class(data) == "data.frame")) stop("Argument data must be a data.frame.")
   if(!is.numeric(dimensions) || length(dimensions) != 2 || !all(paste0("dim_",dimensions) %in% names(data))) stop('Argument dimensions must specify integer two indices of existing dimensions in data.')
   if(!is.numeric(label_size) || label_size < 0) stop("Argument label_size must be a positive numeric value.")
-  if(!is.numeric(pt_padding) || pt_padding < 0) stop("Argument pt_padding must be a positive numeric value.")
-  if(!is.numeric(box_padding) || box_padding < 0) stop("Argument box_padding must be a positive numeric value.")
+  if(!is.numeric(point_padding) || point_padding < 0) stop("Argument pt_padding must be a positive numeric value.")
+  if(!is.numeric(label_padding) || label_padding < 0) stop("Argument box_padding must be a positive numeric value.")
   if(!viridis_set %in% c("A","B","C","D","E","F","G","H")) stop('Argument viridis_set must be one of c("A","B","C","D","E","F","G","H").')
   if(!is.numeric(viridis_limits) || length(viridis_limits) != 2 || any(viridis_limits > 1) || any(viridis_limits < 0)) stop('Argument viridis_limits must be a numeric vector of length two with values between 0 and 1')
 
@@ -73,22 +89,19 @@ plot.embedR_tbl <- function(data,
   p = ggplot2::ggplot(data = data,
                       mapping = ggplot2::aes_string(x = x, y = y ,
                                                     color = "color",
+                                                    fill = "color",
                                                     size = "size",
-                                                    label = "label")) +
-    ggplot2::geom_point() + ggplot2::theme_minimal()
+                                                    label = "label"))
 
-  # handle labels
-  if("label" %in% names(data)){
-
-    p = p + ggrepel::geom_text_repel(point.padding = ggplot2::unit(pt_padding,"npc"),
-                                     box.padding = ggplot2::unit(box_padding,"npc"),
-                                     size = label_size,
-                                     min.segment.length = 0,
-                                     max.overlaps = 1000)
-
+  # add points
+  if(is.null(point_outline)){
+    p = p + ggplot2::geom_point(pch = 21)
+    } else {
+    p = p + ggplot2::geom_point(pch = 21, color = point_outline)
     }
 
-  # add colors
+
+  # add size
   if(!is.null(dplyr::enquo(size))){
 
     # variable name
@@ -114,15 +127,56 @@ plot.embedR_tbl <- function(data,
                  ggplot2::scale_color_viridis_c,
                  ggplot2::scale_color_viridis_d)
 
+    # check type
+    fun2 = ifelse(is.numeric(data %>% dplyr::pull(color)),
+                  ggplot2::scale_fill_viridis_c,
+                  ggplot2::scale_fill_viridis_d)
+
     # style colors
-    p = p + fun(option = viridis_set,
+    p = p +
+      fun(option = viridis_set,
                 begin = viridis_limits[1],
                 end = viridis_limits[2],
-                name = name)
+                name = name) +
+      fun2(option = viridis_set,
+           begin = viridis_limits[1],
+           end = viridis_limits[2],
+           name = name)
+
+  }
+
+  # build plot
+  b = ggplot2::ggplot_build(p)
+
+  # handle labels
+  if("label" %in% names(data)){
+
+    # set to zero
+    if(is.null(label_color)) label_color = 0
+
+    # figure out color vector
+    if(label_color >= 0){
+      colors = cmix(b$data[[1]]$fill, "black", rep(label_color, nrow(b$data[[1]])))
+      } else {
+      colors = cmix(b$data[[1]]$fill, "white", rep(abs(label_color), nrow(b$data[[1]])))
+      }
+
+    # plot with custom colors
+    p = p + ggrepel::geom_text_repel(point.padding = ggplot2::unit(point_padding,"npc"),
+                                     box.padding = ggplot2::unit(label_padding,"npc"),
+                                     size = label_size,
+                                     min.segment.length = 0,
+                                     max.overlaps = label_overlaps,
+                                     #bg.color = "white",
+                                     #bg.r = point_outline,
+                                     color = colors,
+                                     segment.size = segment_size,
+                                     segment.linetype = segment_type)
+
     }
 
   # out
-  p
+  p + theme_minimal()
 
   }
 
--- R/utils.R
+++ R/utils.R
@@ -68,3 +68,65 @@ print.embedR = function(x, n = 5, m = 5,...){
 
   }
 
+
+#' Print embedding object
+#'
+#' Generic function \code{print.embedR} shows a print of the embedding object with an abbreviated \code{matrix} and a \code{tibble} of its attributes.
+#'
+#' @param col1,col2 color values as label, rgb or hex.
+#' @param weight a numeric value specifying the amount of weight given to \code{col1} and \code{col2}
+#' @param format a character string specifying the output type. One of \code{c("rgb","hex")}. Default is \code{"hex"}
+#' @param ... further arguments passed to or from other methods.
+#'
+
+cmix = function(col1, col2, weight, format = 'hex'){
+
+  if(ifelse(is.matrix(col1),nrow(col1),length(col1)) != length(weight) &
+     length(col1) != 1){
+    stop('Length of col1 must be either 1 or matching the length of weight')
+  }
+  if(ifelse(is.matrix(col2),nrow(col2),length(col2)) != length(weight) &
+     length(col2) != 1){
+    stop('Length of col1 must be either 1 or matching the length of weight')
+  }
+  if(length(weight) == 1){
+    if(ifelse(is.matrix(col1),nrow(col1),length(col1)) !=
+       ifelse(is.matrix(col2),nrow(col2),length(col2))){
+      stop('If length of weight = 1, number of colors in col1 and col2 must match')
+    }
+  }
+
+  nrows = max(c(ifelse(is.matrix(col1),nrow(col1),length(col1)),
+                ifelse(is.matrix(col2),nrow(col2),length(col2)),
+                length(weight)))
+
+  if(is.character(col1)){
+    if(length(col1) == 1){
+      col1 = grDevices::col2rgb(col1)
+      col1 = matrix(c(col1),ncol=3,nrow=nrows,byrow=T)
+    } else {
+      col1 = t(sapply(col1,grDevices::col2rgb))
+    }
+  } else{
+    col1 = matrix(c(col1),ncol=3,nrow=nrows,byrow=F)
+  }
+  if(is.character(col2)){
+    if(length(col2) == 1){
+      col2 = grDevices::col2rgb(col2)
+      col2 = matrix(c(col2),ncol=3,nrow=nrows,byrow=T)
+    } else {
+      col2 = t(sapply(col2,grDevices::col2rgb))
+    }
+  } else{
+    col2 = matrix(c(col2),ncol=3,nrow=nrows,byrow=F)
+  }
+
+
+  col = col1 * (1-weight) + col2 * weight
+
+  if(format == 'rgb') return(col)
+  if(format == 'hex') return(grDevices::rgb(data.frame(col),maxColorValue = 255))
+  if(!format %in% c('rgb','hex')) stop('Choose either "rgb" or "hex" as format')
+
+  }
+
